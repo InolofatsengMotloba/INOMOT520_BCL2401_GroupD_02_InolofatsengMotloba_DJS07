41d739af97f74d167a5f1d3902c0c39e7e2615d8
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-05-30 15:06:56 +0200

    Created Header component and added basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
+import React from "react"
+import Header from "./components/Header"
 
-function App() {
+export default function App() {
 
   return (
     <>
-     
+      <Header />
     </>
   )
 }
 
-export default App
