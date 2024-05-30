3a4d0fbae0e15a0d02908f72a0910a965fece48a
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-05-31 00:54:21 +0200

    Update Meme.jsx: Change form element into a div element

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -3,32 +3,27 @@ import React from "react";
 export default function Meme() {
   return (
     <main>
-      <form className="form">
-        <div>
-          <label>
-            Top Text
-            <input
-              type="text"
-              placeholder="Shut up"
-              className="form--input"
-            ></input>
-          </label>
-        </div>
+      <div className="form">
+        <label>
+          Top Text
+          <input
+            type="text"
+            placeholder="Shut up"
+            className="form--input"
+          ></input>
+        </label>
 
-        <div>
-          <label>
-            Bottom Text
-            <input
-              type="text"
-              placeholder="and take my money"
-              className="form--input"
-            ></input>
-          </label>
-        </div>
+        <label>
+          Bottom Text
+          <input
+            type="text"
+            placeholder="and take my money"
+            className="form--input"
+          ></input>
+        </label>
 
         <button className="form--button">Get a new meme image 🖼️</button>
-        
-      </form>
+      </div>
     </main>
   );
 }
