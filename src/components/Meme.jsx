318e66c1114b4a9b9a550e057f9a7988389682f6
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-05-31 01:06:33 +0200

    Add MemeData from an API in memesData.jsx & Add function to randomly choose an image

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import memesData from "../memesData";
 
 export default function Meme() {
+  function getMemeImage() {
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    console.log(url);
+  }
+
   return (
     <main>
       <div className="form">
@@ -22,7 +30,9 @@ export default function Meme() {
           ></input>
         </label>
 
-        <button className="form--button">Get a new meme image 🖼️</button>
+        <button className="form--button" onClick={getMemeImage}>
+          Get a new meme image 🖼️
+        </button>
       </div>
     </main>
   );
