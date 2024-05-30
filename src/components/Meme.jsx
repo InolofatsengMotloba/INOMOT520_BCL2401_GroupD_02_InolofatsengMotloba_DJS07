4b0a620df71885ac8fbe9c903612a517cd397574
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-05-31 01:23:02 +0200

    Styled the Meme component and adjusted layout

--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
+  const [memeImage, setMemeImage] = React.useState("");
+
   function getMemeImage() {
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
-    const url = memesArray[randomNumber].url;
-    console.log(url);
+    setMemeImage(memesArray[randomNumber].url);
   }
 
   return (
@@ -34,6 +35,8 @@ export default function Meme() {
           Get a new meme image 🖼️
         </button>
       </div>
+
+      <img src={memeImage} className="meme--image" />
     </main>
   );
 }
