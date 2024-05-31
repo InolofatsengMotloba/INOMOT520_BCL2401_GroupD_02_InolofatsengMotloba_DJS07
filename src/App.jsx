4a04b147033c4c15ed601cbb0345ba61429a873d
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-05-31 09:58:31 +0200

    Add comments to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from "react"
-import Header from "./components/Header"
-import Meme from "./components/Meme";
+import Header from "./components/Header" // Import the Header component
+import Meme from "./components/Meme"; // Import the Meme component
 
+// Define the main App component
 export default function App() {
 
   return (
