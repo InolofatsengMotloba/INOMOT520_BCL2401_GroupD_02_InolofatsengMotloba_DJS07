266b1d726ed5a7716a873652b6d7d5d79d5dce7a
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-05-31 09:52:10 +0200

    Add comments to Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
+// Define a functional component named Header
 export default function Header() {
   return (
+    // Return a header element with a class name of "header"
     <header className="header">
       <img src="./src/images/troll-face.png" className="header--image" />
       <h2 className="header--title">Meme Generator</h2>
